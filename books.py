de40444809283f8ed38a10cb7c2fbae721450d30
Henrique Corte <hmmbcorte@gmail.com>
2026-02-10 12:00:26 +0000

    file no longer needed and removed comments

--- books.py
+++ books.py
@@ -22,26 +22,8 @@ async def createBook(book_request: BookRequest = Body()):
 	new_book = Book(**book_request.model_dump())
 	print(type(new_book))
 	BOOKS.append(findBookById(new_book))
-
-	# some manual validation that did for testing now using Pydantics for doing payload validation
-	""" expected = set(Book.__annotations__.keys())
-	received = set(book_request.keys())
-
-	if received != expected:
-		return False """
-	
-
-	
-	""" for books_propertie in book.keys():
-		print(books_propertie) """
 	
 def findBookById(book: Book):
-
 	book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
-
- 	# if len(BOOKS) > 0:
-	# 	book.id = BOOKS[-1].id + 1
-	# else:
-	# 	book.id = 1
 
 	return book
