6755f3fa8af6d367190e0af8f12c78001929d803
Henrique Corte <hmmbcorte@gmail.com>
2026-02-18 13:23:05 +0000

    Update and Create endpoints

--- Project_3/main.py
+++ Project_3/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path
+from pydantic import BaseModel, Field
+import starlette.status as status
 from typing import Annotated
 from sqlalchemy import inspect
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
 import models
 from models import Todos
 from database import SessionLocal, engine
@@ -18,12 +21,75 @@ def get_db():
 	finally:
 		db.close()
 
+#db dependency injection
+db_dependency = Annotated[Session, Depends(get_db)]
+
+class TodoRequest(BaseModel):
+	title: str = Field(min_length=10)
+	description: str =Field(min_length=10, max_length=1000)
+	priority: int = Field(gt=0, lt=6)
+	complete: bool
+
+
 @app.get("/")
-async def read_all(db: Annotated[Session, Depends(get_db)]):
+async def read_all(db: db_dependency):
 	return db.query(Todos).all()
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+	todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+	if todo_model is not None:
+		return todo_model
+	raise HTTPException(status_code=404, detail='Todo not found.')
+
+@app.post("/todo/", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+	try:
+		todo_model = Todos(**todo_request.model_dump()) # creates an instance of this object
+		db.add(todo_model) # add this object instance to the db (only in memory)
+		db.commit() # commit this change to db (persist in disk/file)
+		db.refresh(todo_model)  # important!
+		return todo_model
+	except SQLAlchemyError as e:
+		db.rollback()
+		raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Database error while creating todo."
+        )
+
+@app.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(
+		db: db_dependency, 
+		todo_request: TodoRequest,
+		todo_id: int = Path(gt=0) 
+	):
+	todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+	if todo_model is None:
+		raise HTTPException(status_code=404, detail='Todo not found.')
+
+	# updates the instance of object to update
+	todo_model.title = todo_request.title
+	todo_model.description = todo_request.description
+	todo_model.priority = todo_request.priority
+	todo_model.complete = todo_request.complete	
+
+	try:
+		db.add(todo_model) # add this object instance to the db (only in memory)
+		db.commit() # commit this change to db (persist in disk/file)
+		db.refresh(todo_model)  # important!
+		return todo_model
+	except SQLAlchemyError as e:
+		db.rollback()
+		raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Database error while updating todo."
+        )
+
+
+################################################################################################
 
 # This a dummy endpoint of sorts to confirm the tables names of the db
 @app.get("/tables")
-async def get_tables(db: Annotated[Session, Depends(get_db)]):
+async def get_tables():
 	inspector = inspect(engine)
 	return inspector.get_table_names()
--- Project_3/models.py
+++ Project_3/models.py
@@ -1,11 +1,13 @@
 from database import Base
 from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
 
 class Todos(Base):
 	__tablename__ = 'todos'
 
-	id = Column(Integer, primary_key=True, index=True)
-	title = Column(String)
-	description = Column(Integer)
-	priority = Column(Integer)
-	complete = Column(Boolean, default=False)
+	#id = Column(Integer, primary_key=True, index=True)
+	id: Mapped[int] = mapped_column(primary_key=True)  
+	title: Mapped[str]
+	description: Mapped[str]
+	priority: Mapped[int]
+	complete: Mapped[bool]
