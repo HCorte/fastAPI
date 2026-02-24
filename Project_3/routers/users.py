ee4751c77426f50c3bd2bd5e15e39aff996350fa
Henrique Corte <hmmbcorte@gmail.com>
2026-02-24 11:44:46 +0000

    confirm that the new password works by validating the previous password is valid.

--- Project_3/routers/users.py
+++ Project_3/routers/users.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
 from models import Users
 from database import SessionLocal
-from .auth import get_current_user, hash_password
+from .auth import get_current_user, hash_password, verify_password
 
 router = APIRouter(
 	prefix='/users',
@@ -15,8 +15,8 @@ router = APIRouter(
 )
 
 class UpdateUserPasswordRequest(BaseModel):
-	#username: str
 	password: str
+	new_password: str = Field(min_length=6)
 
 def get_db():
 	db = SessionLocal()
@@ -30,15 +30,14 @@ db_dependency = Annotated[Session, Depends(get_db)]
 # user dependency injection (token validation)
 user_dependency = Annotated[dict, Depends(get_current_user)]
 
-@router.get("/user/{user_id}", status_code=status.HTTP_200_OK)
+@router.get("/user/", status_code=status.HTTP_200_OK)
 async def get_user(
 		user: user_dependency,
-		db: db_dependency,
-		user_id: int = Path(gt=0)
+		db: db_dependency
 	):
 	if user is None:
 		raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Authentication Failed')
-	user_model = db.query(Users).filter(Users.user_id == user_id)\
+	user_model = db.query(Users).filter(Users.id == user.get('id'))\
 		.first()
 	
 	if user_model is not None:
@@ -46,7 +45,7 @@ async def get_user(
 	raise HTTPException(status_code=404, detail='User not found.')
 
 
-@router.put("/user/{user_id}", status_code=status.HTTP_200_OK)
+@router.put("/user/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def update_user_password(
 		user: user_dependency,
 		db: db_dependency,
@@ -55,16 +54,22 @@ async def update_user_password(
 	):
 	if user is None or user.get('id') != user_id:
 		raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Authentication Failed')
-	#username = update_user_password_request.username
 	password = update_user_password_request.password
+	new_pass = update_user_password_request.new_password
 	user_model = db.query(Users).filter(Users.id == user_id)\
 		.first()
 		#.filter(Users.username == username)\
 	if user_model is None:
 		raise HTTPException(status_code=404, detail='User not found.')
+
+
+	# Confirm that the token password matchs the current password in the database
+	if not verify_password(password,user_model.hashed_password):
+		raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Error on password change')
 	
-	# hash the new password and update the user model
-	hashed_pass = hash_password(password)
+	# hash the new password 
+	hashed_pass = hash_password(new_pass)
+	# Update the user model
 	user_model.hashed_password = hashed_pass
 	try:
 		db.add(user_model)
@@ -77,4 +82,6 @@ async def update_user_password(
 			status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
 			detail="Database error while updating user password."
 		)
+	
+
 	
