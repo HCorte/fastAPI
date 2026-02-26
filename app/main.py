37808ec50842fa584b485a29dd19ef97e67066ae
Henrique Corte <hmmbcorte@gmail.com>
2026-02-26 17:50:37 +0000

    correct working structer

--- app/main.py
+++ app/main.py
@@ -3,12 +3,17 @@ import models
 from database import engine
 from routers import auth, todos, admin, users
 
-
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+@app.get("healthy")
+def healthy():
+	return {"status": "Healthy"}
 
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
 app.include_router(users.router)
+
+
